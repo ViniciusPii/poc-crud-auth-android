eb5db44ca230fddbda46f1f13a43c04b8c06a4a9
ViniciusPii <vinysales@hotmail.com>
2023-06-08 11:22:10 -0300

    feat: Criando o layout de login e testando o loading

--- app/src/main/java/com/example/poccrud/LoginActivity.kt
+++ app/src/main/java/com/example/poccrud/LoginActivity.kt
@@ -2,7 +2,11 @@ package com.example.poccrud
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.example.poccrud.databinding.ActivityLoginBinding
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class LoginActivity : AppCompatActivity() {
     private val binding: ActivityLoginBinding by lazy {
@@ -12,5 +16,20 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        setListeners()
+    }
+
+    private fun setListeners() {
+        binding.loginButton.setOnClickListener {
+            binding.loginButton.isVisible = false
+            binding.loadingButton.isVisible = true
+
+            MainScope().launch {
+                delay(600)
+                binding.loginButton.isVisible = true
+                binding.loadingButton.isVisible = false
+            }
+        }
     }
 }
